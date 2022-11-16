ff5b47f644e1752e2234faa89df621b73d29f082
Abdyl-Aziz <azizshihabov@mail.ru>
2022-11-16 20:13:07 +0300

    feat: add dark mode

--- script.js
+++ script.js
@@ -2,6 +2,32 @@ const burgerEl = document.querySelector('.nav__burger');
 const navEl = document.querySelector('nav');
 const navList = document.querySelector('.nav__list');
 const body = document.querySelector('body');
+const themeInput = document.querySelector('#themeInput');
+const image1 = document.querySelector('#image1');
+const image2 = document.querySelector('#image2');
+const image3 = document.querySelector('#image3');
+const toggleText = document.querySelector('.toggle-text');
+
+themeInput.addEventListener('click', () => {
+	const htmlEl = document.documentElement;
+	if (htmlEl.dataset.theme !== 'dark') {
+		toggleText.textContent = 'Dark Mode';
+		htmlEl.setAttribute('data-theme', 'dark');
+		imageThemeChange('dark');
+		document.querySelector('.fa-sun').className = 'fas fa-moon';
+	} else {
+		toggleText.textContent = 'Light Mode';
+		htmlEl.removeAttribute('data-theme');
+		imageThemeChange('light');
+		document.querySelector('.fa-moon').className = 'fas fa-sun';
+	}
+});
+
+function imageThemeChange(theme) {
+	image1.src = `img/undraw_proud_coder_${theme}.svg`;
+	image2.src = `img/undraw_feeling_proud_${theme}.svg`;
+	image3.src = `img/undraw_conceptual_idea_${theme}.svg`;
+}
 
 burgerEl.addEventListener('click', () => {
 	if (burgerEl.classList.contains('active')) {
